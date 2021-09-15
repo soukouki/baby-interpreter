328021b11b6c3fa1611f385ced6868a9349271fc
soukouki <soukouki0@yahoo.co.jp>
2021-09-15 20:14:39 +0900

    字句解析終わり

---
+++ interpriter.js
@@ -0,0 +1,47 @@
+function isDigit(char) {
+  const charCode = char.charCodeAt(0)
+  return '0'.charCodeAt(0) <= charCode && charCode <= '9'.charCodeAt(0)
+}
+
+function countDigitsFromSource(source) {
+  let readPosition = 0
+  while (readPosition < source.length) {
+    if (!isDigit(source[readPosition])) {
+      return readPosition
+    }
+    readPosition += 1
+  }
+  return readPosition
+}
+
+module.exports.lexicalAnalyse = function (source) {
+  const tokens = []
+  let readPosition = 0
+  while (readPosition < source.length) {
+    switch (source[readPosition]) {
+      case '+':
+        tokens.push({
+          type: 'Add',
+        })
+        readPosition += 1
+        break
+      default:
+        if (isDigit(source[readPosition])) {
+          const digitsCount = countDigitsFromSource(source.substring(readPosition))
+          tokens.push({
+            type: 'Int',
+            value: parseInt(source.substring(readPosition, readPosition + digitsCount), 10),
+          })
+          readPosition += digitsCount
+        } else {
+          // 不明な文字
+          tokens.push({
+            type: 'UnknownCharacter',
+            value: source[readPosition],
+          })
+          readPosition += 1
+        }
+    }
+  }
+  return tokens
+}
--- interpriter.test.js
+++ interpriter.test.js
@@ -1,5 +1,26 @@
 /* eslint-disable no-undef */
 
-test('最初のテスト', () => {
-  expect(123).toBe(123)
+const interpriter = require('./interpriter')
+
+const { lexicalAnalyse } = interpriter
+
+describe('字句解析', () => {
+  test('1', () => {
+    expect(lexicalAnalyse('1')).toStrictEqual([{ type: 'Int', value: 1 }])
+  })
+  test('123', () => {
+    expect(lexicalAnalyse('123')).toStrictEqual([{ type: 'Int', value: 123 }])
+  })
+  test('+', () => {
+    expect(lexicalAnalyse('+')).toStrictEqual([{ type: 'Add' }])
+  })
+  test('1+2', () => {
+    expect(lexicalAnalyse('1+2')).toStrictEqual([
+      { type: 'Int', value: 1 },
+      { type: 'Add' },
+      { type: 'Int', value: 2 }])
+  })
+  test('無効な文字列', () => {
+    expect(lexicalAnalyse('あ')).toStrictEqual([{ type: 'UnknownCharacter', value: 'あ' }])
+  })
 })
